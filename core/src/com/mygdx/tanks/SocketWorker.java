90ccfb646c7a3a3de96c345217691aec74987396
KB1101 <kornelgcc@gmail.com>
2016-06-16 00:23:11 +0200

    wysylanie do serwera, brak odbioru

--- core/src/com/mygdx/tanks/SocketWorker.java
+++ core/src/com/mygdx/tanks/SocketWorker.java
@@ -16,6 +16,8 @@ public class SocketWorker implements Runnable {
     private OutputStream outputStream;
     private DataInputStream dis;
     private DataOutputStream dos;
+    private BufferedReader bufIn;
+    private BufferedWriter bufOut;
 
     public SocketWorker(Socket socket, Board board){
         this.socket = socket;
@@ -26,16 +28,23 @@ public class SocketWorker implements Runnable {
         try{
             this.inputStream = this.socket.getInputStream();
             this.outputStream = this.socket.getOutputStream();
-            this.dis = new DataInputStream(inputStream);
-            this.dos = new DataOutputStream(outputStream);
+           // this.dis = new DataInputStream(inputStream);
+           // this.dos = new DataOutputStream(outputStream);
+            this.bufIn = new BufferedReader( new InputStreamReader( this.inputStream ) );
+            this.bufOut = new BufferedWriter( new OutputStreamWriter( this.outputStream ) );
+
+
             SocketAddress sockaddr = this.socket.getRemoteSocketAddress();
 
             System.out.println("Nawiązano połaczenie z: " + sockaddr.toString() );
-            process(dis,dos);
+            process(bufIn,bufOut);
+            //process(dis,dos);
             System.out.println("Klient zakończył połaczenie: " + sockaddr.toString() );
 
-            dos.close();
-            dis.close();
+           // dos.close();
+            //dis.close();
+            bufOut.close();
+            bufIn.close();
             inputStream.close();
             outputStream.close();
 
@@ -46,6 +55,52 @@ public class SocketWorker implements Runnable {
         }
     }
 
+
+    private void process(BufferedReader bufIn, BufferedWriter bufOut ){
+        try {
+            char[] buffer = new char[1024];
+            int bytes = 0;
+
+
+            String str;
+            str = bufIn.readLine();
+            int uid = Integer.parseInt(str);
+
+
+            //String suid = Integer.toString(uid);
+            //dos.writeBytes(suid+"\n\r");
+            //System.out.write(buffer, 0, bytes);
+
+
+
+            while ((bytes = bufIn.read(buffer)) != -1) {
+                System.out.print(buffer);
+                System.out.println(" ");
+                //  akcje
+            }
+
+        } catch (Exception ex){
+            System.out.println(ex);
+        } finally {
+            //this.magazyn.delUser();
+        }
+    }
+
+
+    public void send(String str){
+        try {
+            //this.dos.writeBytes(str);
+            this.bufOut.write(str);
+            this.bufOut.newLine();
+            this.bufOut.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+        // na razie nieaktualnie
     private void process(DataInputStream dis,DataOutputStream dos ){
         try {
             byte[] buffer = new byte[1024];
@@ -75,11 +130,6 @@ public class SocketWorker implements Runnable {
             //this.magazyn.delUser();
         }
     }
-    public void send(String str){
-        try {
-            this.dos.writeBytes(str);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
+
 }
